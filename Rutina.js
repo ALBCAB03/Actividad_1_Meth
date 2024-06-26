70c976ca0d426e9a4eaf92fbd244511a66f64d75
Alberto <albertocm92@hotmail.com>
2024-06-26 16:11:11 -0500

    Update Rutina.js

--- Rutina.js
+++ Rutina.js
@@ -6,3 +6,14 @@ var depertar = function (hora, dia) {
 this.hora = 6;
 this.dia = miercoles;
 }
+//Ejercicio 4
+var ejercicios = function (name, horas) {
+    this.name = "Smartfit";
+    this.horas = 2;
+    }
+
+//Ejercicio 6
+var desayuno = function (comida, hora) {
+    this.comida = "Buñuelos";
+    this.horas = "10:30";
+    }
