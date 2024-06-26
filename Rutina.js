d28b135bb0d4ae3037c6be7c96acb3b165764b72
Alberto <albertocm92@hotmail.com>
2024-06-26 14:58:27 -0500

    Update Rutina.js

--- Rutina.js
+++ Rutina.js
@@ -1,2 +1,8 @@
 console.log("Rutina Diaria")
-console.log("Hora de despertar")
+console.log("Dia de la semana")
+
+//Ejercicio 3
+var depertar = function (hora, dia) {
+this.hora = 6;
+this.dia = miercoles;
+}
